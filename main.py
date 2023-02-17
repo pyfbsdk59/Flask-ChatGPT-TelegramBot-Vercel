ee65f7d681ef2a79442475e18f291f3c4b21ed6d
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-02-17 14:38:59 +0800

    indent

--- main.py
+++ main.py
@@ -25,20 +25,20 @@ LANGUAGE_TABLE = {
 
 
 class Prompts:
-	    def __init__(self):
-	        self.msg_list = []
-	        self.msg_list.append(f"AI:{LANGUAGE_TABLE[chat_language]}")
+    def __init__(self):
+        self.msg_list = []
+        self.msg_list.append(f"AI:{LANGUAGE_TABLE[chat_language]}")
 	    
-	    def add_msg(self, new_msg):
-	        if len(self.msg_list) >= MSG_LIST_LIMIT:
-	            self.remove_msg()
-	        self.msg_list.append(new_msg)
+    def add_msg(self, new_msg):
+        if len(self.msg_list) >= MSG_LIST_LIMIT:
+            self.remove_msg()
+        self.msg_list.append(new_msg)
 	
-	    def remove_msg(self):
-	        self.msg_list.pop(0)
+    def remove_msg(self):
+        self.msg_list.pop(0)
 	
-	    def generate_prompt(self):
-	        return '\n'.join(self.msg_list)	
+    def generate_prompt(self):
+        return '\n'.join(self.msg_list)	
 	
 class ChatGPT:  
     def __init__(self):
