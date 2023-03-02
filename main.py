381bb40a08394814885f2bb5f4d9c5dc48e2d6b5
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-03-02 15:54:10 +0800

    real gpt3.5 model api11

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class ChatGPT:
   {'role': 'system', 'content': 'You are a helpful assistant that translates English to French.'},
   {'role': 'user', 'content': 'Translate the following English text to French: "{text2}"'}
 ]
-	            prompt=self.prompt.generate_prompt(),
+	            #prompt=self.prompt.generate_prompt(),
 	            #temperature=self.temperature,
 	            #frequency_penalty=self.frequency_penalty,
 	            #presence_penalty=self.presence_penalty,
